d63eb7fa816a97b6dbe6808a3fd2117b104d8825
mridubhatnagar <mridubhatnagar@rediffmail.com>
2024-07-10 23:02:54 +0530

    improved approach

--- go-practice-problems/integer_to_binary.go
+++ go-practice-problems/integer_to_binary.go
@@ -6,14 +6,18 @@ func main() {
 	fmt.Println("Program to convert Integer to binary")
 	fmt.Println("Input the number: ")
 	fmt.Scan(&inputNumber)
-	var output = []int{}
+	var output string = ""
 	var remainder int 
-	var quotient int 
+	var quotient int
 	for inputNumber > 0 {
-		remainder = inputNumber%2 
+		remainder = inputNumber%2
+		if remainder == 1 {
+			output = "1" + output
+		} else {
+			output = "0" + output
+		}
 		quotient = inputNumber/2
 		inputNumber = quotient
-		output = append(output, remainder)
 	}
-	fmt.Printf("%v", output)
+	fmt.Printf("%s", output)
 }
